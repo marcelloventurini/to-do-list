fe8d0f4d4d6ec41621e1f8a0ce410674cd6ad887
Marcello Venturini <marcellofventurini@gmail.com>
2023-10-13 11:41:42 -0300

    feat: create task schema

---
+++ src/models/task.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document } from "mongoose"
+
+export interface ITask extends Document {
+  title: string
+  description?: string
+  dueDate?: Date
+  priority?: string
+  status?: string
+}
+
+const taskSchema: Schema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  dueDate: Date,
+  priority: {
+    type: String,
+    enum: ['low', 'medium', 'high'],
+    default: 'medium'
+  },
+  status: {
+    type: String,
+    enum: ['todo', 'inProgress', 'completed'],
+    default: 'todo'
+  }
+})
+
+export default mongoose.model<ITask>('task', taskSchema)
